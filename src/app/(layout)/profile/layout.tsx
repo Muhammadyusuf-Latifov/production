b6a2ef3fdd1f758b9b643c0cc0db0799ace3b57f
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-09-23 17:14:05 +0500

    ffdfjidf

--- src/app/(layout)/profile/layout.tsx
+++ src/app/(layout)/profile/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="Layout">
       <Me />
-      <h2>Layout</h2>
+      
       {children}
     </div>
   );
--- src/app/(layout)/profile/page.tsx
+++ src/app/(layout)/profile/page.tsx
@@ -2,8 +2,10 @@ import { memo } from "react";
 
 const Profile = () => {
   return (
-    <div className="Profile min-h-[500px]">
-      <h2>Profile </h2>
+    <div className="w-[100%] min-h-[400px] flex items-center justify-center">
+      <div>
+        profile
+    </div>
     </div>
   );
 };
